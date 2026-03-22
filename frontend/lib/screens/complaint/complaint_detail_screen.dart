f60410fc06170c713c5c19e2053f5dd57e2aadb4
Kavindu Lishan Gunarathna <kavindulishan2004@gmail.com>
2026-03-22 11:49:54 +0530

    Create complaint_detail_screen.dart

---
+++ frontend/lib/screens/complaint/complaint_detail_screen.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:urbancare_frontend/models/complaint.dart';
+import 'package:urbancare_frontend/repositories/complaint_repository.dart';
+import 'package:urbancare_frontend/widgets/primary_button.dart';
